8b009f80e5e1187dfb477bca9d90686a2aea39d1
jacobrphillips <0123phillips@gmail.com>
2023-08-07 18:57:30 +0100

    Creates a list with all previous calculation totals and gives the user the ability to delete the list. It also allows the user to use the results in the list to perform new calculations.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,9 +1,14 @@
-﻿class Program
+﻿using System;
+using System.Collections.Generic;
+
+class Program
 {
     static void Main(string[] args)
     {
         bool endApp = false;
         int numberOfTimesUsed = 0;
+        List<double> listOfAllCalculations = new List<double>();
+        double num1 = 0;
 
         // Display title as the C# console calculator app.
         Console.WriteLine("Console Calculator in C#\r");
@@ -11,49 +16,45 @@
 
         while (!endApp)
         {
-            // Declare variables and then initialize to zero.
-            double num1 = 0;
-            double num2 = 0;
             double result = 0;
-
-
             // Boolean value to use as the while loop's conditional.
             bool run = true;
             while (run)
             {
-                
-
-                // Ask the user to type the first number.
-                Console.WriteLine("Type a number, and then press Enter");
-
-                // Record user input from Console into string variable.
-                string input1 = Console.ReadLine();
-
-                // If the user's input can be converted from string to double, then the boolean value for the while loop's conditional is set to false, ending the loop. 
-                // Else, it notifies the user to please enter a numerical value.
-                if (double.TryParse(input1, out num1))
+                if (num1 > 0)
                 {
                     run = false;
-                    break;
                 }
                 else
                 {
-                    Console.WriteLine("Please enter a numerical value.");
+                    // Ask the user to type the first number.
+                    Console.WriteLine("Type a number, and then press Enter");
+                    string input1 = Console.ReadLine();
+
+                    // If the user's input can be converted from string to double, then the boolean value for the while loop's conditional is set to false, ending the loop. 
+                    // Else, it notifies the user to please enter a numerical value.
+                    if (double.TryParse(input1, out num1))
+                    {
+                        run = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a numerical value.");
+                    }
                 }
             }
 
             // Ask the user to type the second number.
             bool run2 = true;
+            double num2 = 0;
             while (run2)
             {
                 Console.WriteLine("Type another number, and then press Enter");
-
                 string input2 = Console.ReadLine();
 
                 if (double.TryParse(input2, out num2))
                 {
                     run2 = false;
-                    break;
                 }
                 else
                 {
@@ -61,16 +62,13 @@
                 }
             }
 
-
-            // Ask the user to choose an option.
             Console.WriteLine("Choose an option from the following list:");
             Console.WriteLine("\ta - Add");
             Console.WriteLine("\ts - Subtract");
             Console.WriteLine("\tm - Multiply");
             Console.WriteLine("\td - Divide");
             Console.Write("Your option? ");
-
-            string operation = Console.ReadLine();
+            string operation = Console.ReadLine().Trim().ToLower();
             numberOfTimesUsed++;
 
             try
@@ -79,28 +77,80 @@
                 if (double.IsNaN(result))
                 {
                     Console.WriteLine("This operation will result in a mathematical error.\n");
-                }else
+                }
+                else
                 {
                     Console.WriteLine("Your result: {0}", result);
-
+                    listOfAllCalculations.Add(result);
+                    Console.WriteLine($"Your result has been added to your recent calculations list.");
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("Oh no! An exception error occured trying to do the math.\n - Details " + e.Message);
+                Console.WriteLine("Oh no! An exception error occurred trying to do the math.\n - Details " + e.Message);
             }
-            Console.WriteLine($"The calculator has been used {numberOfTimesUsed} number of times");
-            Console.WriteLine("------------------------------\n");
 
-            // Wait for the user to respond before closing 
-            Console.WriteLine("Press 'n' and Enter to close the app, or press any other key and Enter to continue: " );
-            if(Console.ReadLine() == "n")
+            Console.WriteLine($"The calculator has been used {numberOfTimesUsed} number of times");
+
+            // Ask if the user wants to continue or exit the app
+            Console.WriteLine("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            string exitInput = Console.ReadLine().Trim().ToLower();
+            if (exitInput == "n")
             {
                 endApp = true;
             }
             Console.WriteLine("\n");
+
+            // Check if there are any previous calculations to show
+            if (listOfAllCalculations.Count > 0)
+            {
+                Console.WriteLine("Would you like to view your previous calculation totals? Press 'y' then Enter to proceed, if not then press any key to continue to use the calculator.");
+                string viewPreviousTotals = Console.ReadLine().Trim().ToLower();
+
+                if (viewPreviousTotals == "y")
+                {
+                    Console.WriteLine("Here are your previous calculations:");
+                    double[] arrayOfAllCalculations = listOfAllCalculations.ToArray();
+                    int placeInList;
+
+                    for (int i = 0; i < arrayOfAllCalculations.Length; i++)
+                    {
+                        placeInList = i + 1;
+                        Console.WriteLine($"{placeInList}. {arrayOfAllCalculations[i]}");
+                    }
+
+                    Console.WriteLine("\nWould you like to use a specific calculation total in your new calculation? Press 'o' for yes, 'p' for no.");
+                    string decisionsAboutList = Console.ReadLine().Trim().ToLower();
+                    if (decisionsAboutList == "o")
+                    {
+             
+                        Console.WriteLine("Enter the index of the calculation total you want to use:");
+                        string selectedIndex = Console.ReadLine();
+                        if (int.TryParse(selectedIndex, out int index) && index > 0 && index <= arrayOfAllCalculations.Length)
+                        {
+                            num1 = arrayOfAllCalculations[index - 1];
+                            Console.WriteLine($"Number 1 updated to the selected total: {num1}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter a valid index of the calculation total.");
+                        }
+                    }
+                    else if (decisionsAboutList == "p")
+                    {
+                        num1 = 0;
+                        Console.WriteLine("\nWould you like to delete your previous calculations? Press 'y' to delete, Press 'n' to go back to the calculator.");
+                        decisionsAboutList = Console.ReadLine().Trim().ToLower();
+
+                        if (decisionsAboutList == "y")
+                        {
+                            listOfAllCalculations.Clear();
+                            Console.WriteLine("You have deleted your previous calculations.");
+                        }
+                    }
+                }
+            }
         }
-        return;
     }
 }
 
@@ -136,6 +186,5 @@ class Calculator
                 break;
         }
         return result;
-
     }
 }
